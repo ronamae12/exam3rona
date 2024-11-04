75a1a5b0a8706fdeb81d8465a17e3e18400c3f4a
Rona Mae Ancheta <ronamaeancheta18@gmail.com>
2024-11-04 19:02:41 +0800

    Initial Commit

---
+++ userAge.js
@@ -0,0 +1,15 @@
+let age = 19; 
+
+if (age < 5) {
+    console.log("You are a Toddler.");
+} else if (age >= 5 && age <= 12) {
+    console.log("You are a Child.");
+} else if (age >= 13 && age <= 19) {
+    console.log("You are a Teenager.");
+} else if (age >= 20 && age <= 35) {
+    console.log("You are a Young Adult.");
+} else if (age >= 36 && age <= 60) {
+    console.log("You are Middle-Aged.");
+} else {
+    console.log("You are a Senior.");
+}
